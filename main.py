d45a134ca904129a2ca4ecfaf1682722b940d37e
shewonders <shestillwonders@yahoo.com>
2022-09-06 16:22:02 +0300

    lesson3

--- main.py
+++ main.py
@@ -19,8 +19,32 @@
 #     print(f"Personal info: {fname} {lname}, born in {yob}, resides in {city}. Their e-mail is {email}, and their telephone number is {tel}")
 # yellowpages()
 
-#task 3
-ggg
+#task 3 for any number of elements
+def my_func(x, y, z):
+    l = list(input("Type x, y and z, separated by spaces"))
+    n = 0
+    while n < len(l):
+        if l[n] > l[n+1]:
+            y = l[n]
+            break
+        else:
+            y = l[n+1]
+            n = n + 1
+    l.remove(y)
+    while n < len(l):
+        if l[n] > l[n + 1]:
+            y1 = l[n]
+            break
+        else:
+            y1 = l[n + 1]
+            n = n + 1
+    return y + y1
+print(f"The sum of the two largest numbers in your input, {y} and {y1}, is {my_func}")
+
+
+
+
+
 
 
 
