fd276176933a92f35f2c577584f9666726399c63
shewonders <shestillwonders@yahoo.com>
2022-09-06 16:22:02 +0300

    lesson3

---
+++ main.py
@@ -0,0 +1,29 @@
+#task 1
+# def mydivision():
+#     x = int(input("Type the numerator:"))
+#     y = int(input("Type the denominator:"))
+#     if y == 0:
+#         print("No division by O, type the arguments again")
+#     else:
+#         print(f"{x} divided by {y} is {x/y}")
+# mydivision()
+
+#task 2
+# def yellowpages():
+#     lname = input("Type a person's last name ")
+#     fname = input("Type the person's first name ")
+#     yob = input("Type the person's year of birth ")
+#     city = input("Type their city of residence ")
+#     email = input("Now, type their email ")
+#     tel = input("Lastly, type their telephone number ")
+#     print(f"Personal info: {fname} {lname}, born in {yob}, resides in {city}. Their e-mail is {email}, and their telephone number is {tel}")
+# yellowpages()
+
+#task 3
+ggg
+
+
+
+
+
+
